29ac51717a68aa044b725790967050725080b2ec
moataz in unix <Moataz.Negm@gmail.com>
2014-10-06 01:54:34 +0200

    DiskGroup is done .. no destroy

---
+++ jsonthis2.sh
@@ -0,0 +1,10 @@
+#! /usr/local/bin/zsh
+# needed the operands to be like : one vaule1 two value2 three value3 four value4
+oper=($@);
+streamo=`echo '{"'$oper[1]'":"'$oper[2]`;
+for (( c=3;c<$#;c+=2 ));
+do
+	streamo=`echo $streamo,'"'$oper[$c]'"':'"'$oper[$(($c+1))]'"'`;
+done
+streamo=`echo $streamo"}"`;
+echo $streamo;
