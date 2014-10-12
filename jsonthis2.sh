809841d4427eb8192aa0a3d9f1f6f8bbd4c390d1
moataz in unix <Moataz.Negm@gmail.com>
2014-10-12 22:12:25 +0200

    finished Snapshots

--- jsonthis2.sh
+++ jsonthis2.sh
@@ -3,10 +3,10 @@
 str=$@;
 oper=($(echo ${str}));
 n=`echo ${#oper[@]}`
-streamo=`echo '{"'$oper[1]'":"'$oper[2]'"'`;
+streamo=`echo '{"'$oper[2]'":"'$oper[1]'"'`;
 for (( c=3;c<$n;c+=2 ));
 do
-	streamo=`echo $streamo,'"'$oper[$c]'"':'"'$oper[$(($c+1))]'"'`;
+	streamo=`echo $streamo,'"'$oper[$(($c+1))]'"':'"'$oper[$c]'"'`;
 done
 streamo=`echo $streamo"}"`;
 echo $streamo ;
