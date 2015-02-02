85e02d5a1efe11660db368e7987541cdf41d1477
abdo-farag <abdofarag85@gmail.com>
2015-02-02 22:59:47 +0200

    search scripts

---
+++ io-scripts/json/search/searchdate.sh
@@ -0,0 +1,16 @@
+#!/usr/local/bin/zsh
+device=$1
+date=$2
+search1=`./searchdevice.sh $device`
+search1=`echo $search1 | awk 'NR==4{printf"%s",$1}'`
+json=`echo $search1 | jq -c '.[]'| awk 'NR==2{printf"%s",$1}'|jq -c '.[]|.[]|.[]'`
+json1=`echo "{\"name\":\"$device\",\"stats\":[{\"Dates\":[\n"$json"\n]}]}"`
+search=`echo $json1 | grep $date`
+pre=`echo $json1 | grep -B9999999 $date| grep -v $date | tr "\n" "," | sed 's:\[,:\[:'` 
+post=`echo $json1 | grep -A9999999 $date| grep -v $date| tr "\n" ","|sed 's:^:,:'|sed 's:,]}]},$:]}]}:'`
+echo "===================== pre ==================="
+echo $pre
+echo "=================== search =================="
+echo $search
+echo "===================== post ==================="
+echo $post
---
+++ io-scripts/json/search/searchtime.sh
@@ -0,0 +1,17 @@
+#!/usr/local/bin/zsh
+device=$1
+date=$2
+Time=$3
+search2=`./searchdate.sh $device $date`
+search2=`echo $search2 | awk 'NR==4{printf"%s",$1}'`
+json=`echo $search2 | jq -c '.[]'| awk 'NR==2{printf"%s",$1}'|jq -c '.[]'`
+json1=`echo "{\"Date\":\"$date\",\"times\":[\n"$json"\n]}"`
+search=`echo $json1 | grep $Time`
+pre=`echo $json1 | grep -B9999999 $Time| grep -v $Time | tr "\n" "," | sed 's:\[,:\[:'` 
+post=`echo $json1 | grep -A9999999 $Time| grep -v $Time| tr "\n" ","|sed 's:^:,:'|sed 's:,]},$:]}:'`
+echo "===================== pre ==================="
+echo $pre
+echo "=================== search =================="
+echo $search
+echo "===================== post ==================="
+echo $post
