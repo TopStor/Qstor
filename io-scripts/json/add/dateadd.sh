f4f99949dece0acdb2d9f731ca3607f4d38c43b4
abdo-farag <abdofarag85@gmail.com>
2015-02-13 06:22:28 +0200

    io-scripts

--- io-scripts/json/add/dateadd.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/local/bin/zsh
-str=$@
-str="$str"
-oper=($(echo ${str}));
-end=`sed "s/]}/]},$oper/" currenttmp.log`
-echo $end > currenttmp.log 
--- io-scripts/json/add/timeadd.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/local/bin/zsh
-str=${@}
-oper=($(echo ${str}));
-end=`sed "s/]/,$oper]/" currenttmp.log`
-echo $end > currenttmp.log
