4540348496d701b2d37381b5a30f3e8ccbe92fc2
abdo-farag <abdofarag85@gmail.com>
2015-02-21 12:30:39 +0200

    update

--- update/genpatch.sh
+++ update/genpatch.sh
@@ -1,6 +1,7 @@
 #!/usr/local/bin/zsh
-cd /root/Qstor/update
-git checkout update
+cd /root/update
+git add *
+git commit -a -m 'add'
 git format-patch -1
 tar -cvzf /patches.tar.gz *.patch
 rm -rf *.patch 
