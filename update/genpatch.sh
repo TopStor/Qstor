deea9ed059f5fc3407218a682448f9be7e1e20a5
abdo-farag <abdofarag85@gmail.com>
2015-02-25 19:59:06 +0200

    update

--- update/genpatch.sh
+++ update/genpatch.sh
@@ -1,8 +1,7 @@
 #!/usr/local/bin/zsh
 cd /root/update
-git format-patch -1
+git format-patch -100 HEAD --stdout > 0001-last-100-commits.patch
 tar -cvzf /update.tar.gz *.patch
 gpg --sign /update.tar.gz
 mv /update.tar.gz.gpg /qstor.bin 
 rm -rf *.patch /update.tar.gz
-exit 0
