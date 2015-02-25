3222630cab01994140d64d7ad22e53dae1c92eca
abdo-farag <abdofarag85@gmail.com>
2015-02-25 14:24:55 +0200

    update

--- update/genpatch.sh
+++ update/genpatch.sh
@@ -3,6 +3,8 @@ cd /root/update
 git add *
 git commit -a -m 'update'
 git format-patch -1
-tar -cvzf /update.tar.gz *.patch
-rm -rf *.patch 
+tar -cvzf update.tar.gz *.patch
+gpg --sign update.tar.gz
+mv update.tar.gz.gpg /qstor.bin 
+rm -rf *.patch update.tar.gz
 exit 0
--- update/updatebranch.sh
+++ update/updatebranch.sh
@@ -1,9 +1,12 @@
 #!/usr/local/bin/zsh
+pass=$1
 cd /scripts
 git init 
 git add *
 git commit -a -m 'add'
 git checkout -b hotfix
+gpg -d -o /update.tar.gz /update.tar.gz.gpg
+echo $? >> /dev/null
 mv /update.tar.gz /scripts
 tar -xvzf update.tar.gz
 rm -rf update.tar.gz
@@ -13,4 +16,3 @@ git merge hotfix
 git branch -D hotfix
 rm -rf .git
 rm -rf *-update.patch
-
