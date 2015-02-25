fe47a3be76f6a9cb78c7ed8e71f776c8464cce5d
abdo-farag <abdofarag85@gmail.com>
2015-02-25 19:54:50 +0200

    update

--- update/genpatch.sh
+++ update/genpatch.sh
@@ -1,10 +1,8 @@
 #!/usr/local/bin/zsh
 cd /root/update
-git add *
-git commit -a -m 'update'
 git format-patch -1
-tar -cvzf update.tar.gz *.patch
-gpg --sign update.tar.gz
-mv update.tar.gz.gpg /qstor.bin 
-rm -rf *.patch update.tar.gz
+tar -cvzf /update.tar.gz *.patch
+gpg --sign /update.tar.gz
+mv /update.tar.gz.gpg /qstor.bin 
+rm -rf *.patch /update.tar.gz
 exit 0
--- update/updatebranch.sh
+++ update/updatebranch.sh
@@ -1,18 +1,30 @@
 #!/usr/local/bin/zsh
-pass=$1
+gpg --verify /qstor.bin > /dev/null 2>&1
+if [ $? = 0 ]
+then
+echo "Good Update File"
 cd /scripts
-git init 
-git add *
-git commit -a -m 'add'
-git checkout -b hotfix
-gpg -d -o /update.tar.gz /update.tar.gz.gpg
-echo $? >> /dev/null
-mv /update.tar.gz /scripts
-tar -xvzf update.tar.gz
-rm -rf update.tar.gz
-git am *-update.patch
-git checkout master
-git merge hotfix
-git branch -D hotfix
+git init > /dev/null 2>&1 
+git add * > /dev/null 2>&1
+git commit -a -m 'add' > /dev/null 2>&1
+git checkout -b hotfix > /dev/null 2>&1
+gpg -d -o /update.tar.gz /qstor.bin > /dev/null 2>&1
+mv /update.tar.gz /scripts > /dev/null 2>&1
+tar -xvzf update.tar.gz > /dev/null 2>&1
+rm -rf update.tar.gz > /dev/null 2>&1
+git am *.patch > /dev/null 2>&1
+git checkout master > /dev/null 2>&1
+out=`git merge hotfix`
+echo $out | grep Updating  > /dev/null 2>&1
+if [ $? -ne 1 ]
+then
+echo "Update Successfully."
+else
+echo "Already up-to-date."
+fi
+git branch -D hotfix > /dev/null 2>&1
 rm -rf .git
-rm -rf *-update.patch
+rm -rf *.patch
+else
+echo "Bad Update File"
+fi
